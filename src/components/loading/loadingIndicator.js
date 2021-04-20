47a087678d67a4bc249f4270357df68fa727a7bf
younggg96 <yangguannggeng960123@gmail.com>
2021-04-20 15:02:04 -0400

    fix bug

--- src/components/loading/loadingIndicator.js
+++ src/components/loading/loadingIndicator.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Spinner } from "@ui-kitten/components";
+import { View } from "react-native";
+import { styles } from "../../styles/userAuth/landingStyle";
 
 export const LoadingIndicator = (props) => (
-    <View style={[props.style, styles.indicator]}>
-        <Spinner status='basic' size='small' />
-    </View>
+  <View style={[props.style, styles.indicator]}>
+    <Spinner status='basic' size='small' />
+  </View>
 );
--- src/views/userAuth/landing.js
+++ src/views/userAuth/landing.js
@@ -3,22 +3,23 @@ import React from 'react';
 import { Button, Spinner, Text } from "@ui-kitten/components";
 import { Image, View } from 'react-native';
 import { styles } from '../../styles/userAuth/landingStyle';
+import { LoadingIndicator } from '../../components/loading/loadingIndicator';
 
-const LoadingIndicator = (props) => (
-  <View style={[props.style, styles.indicator]}>
-    <Spinner status='basic' size='small' />
-  </View>
-);
+// const LoadingIndicator = (props) => (
+//   <View style={[props.style, styles.indicator]}>
+//     <Spinner status='basic' size='small' />
+//   </View>
+// );
 
 const Landing = ({ navigation }) => {
   const [spinner, setSpinner] = React.useState(false);
   
   const onStart = () => {
+    setSpinner(true);
     setTimeout(() => {
-      setSpinner(true)
       navigation.navigate("SignIn");
+      setSpinner(false);
     }, 1000);
-    setSpinner(false)
   };
 
   return (
@@ -26,7 +27,7 @@ const Landing = ({ navigation }) => {
       <Image source={require("../../../assets/img/logo.png")} style={styles.logo} />
       <Text style={styles.title} category="h5">Flexibility. Availability. Reliability.</Text>
       <Text style={styles.subTitle} category="s1">We are everything real estate. Let us share it with you!</Text>
-      {spinner ?
+      {!spinner ?
         <Button style={styles.button} onPress={onStart}>
           Let's Start
         </Button> :
