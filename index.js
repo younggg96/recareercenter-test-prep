e644066186a07315e6f6a7ae586471633c8dab4a
younggg96 <yangguannggeng960123@gmail.com>
2021-09-13 19:05:43 -0400

    new update

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+// import * as InAppPurchases from 'expo-in-app-purchases';
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+// import {
+//   connectAsync,
+//   setPurchaseListener,
+//   finishTransactionAsync,
+//   IAPResponseCode,
+// } from "expo-in-app-purchases";
+// import { Alert } from 'react-native';
 
+// (function init() {
+//   // try {
+//   const res = connectAsync();
+//   res.then((data) => {
+
+//     Alert.alert(data)
+//   })
+//   // } catch (err) {
+//   //   Alert.alert("Error occurred: " + JSON.stringify(err));
+//   // }
+// })();
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
