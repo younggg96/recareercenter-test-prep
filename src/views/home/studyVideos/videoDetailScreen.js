0bd6492064f3a7d760556bde8e75e1090b11f027
younggg96 <yangguannggeng960123@gmail.com>
2022-12-11 23:42:21 -0800

    fix video no sound issue

--- src/views/home/studyVideos/videoDetailScreen.js
+++ src/views/home/studyVideos/videoDetailScreen.js
@@ -1,22 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View } from "react-native";
 import { Button, Text, TopNavigation } from "@ui-kitten/components";
 // redux
-import { Video } from "expo-av";
+import { Audio, Video } from "expo-av";
 import { BackIcon } from "../../../components/icons/icons";
 import Constants from 'expo-constants';
+import Toast from "react-native-simple-toast";
+
+const triggerAudio = async (ref) => {
+  await Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
+  ref.current.playAsync();
+};
 
 export const VideosDetailScreen = ({ route, navigation }) => {
   const { videoDetail } = route.params;
-
-  const [mounted, setMounted] = useState(false);
+  const [status, setStatus] = useState({});
+  const ref = useRef(null);
 
   useEffect(() => {
-    const id = setTimeout(() => {
-      setMounted(true)
-    }, 1000);
-    return () => clearTimeout(id);
-  })
+    if (status.isBuffering) {
+      Toast.show(
+        "Video is loading...",
+        Toast.SHORT
+      );
+    }
+    if (status.isPlaying) triggerAudio(ref);
+  }, [ref, status.isBuffering, status.isPlaying]);
 
   const navigateBack = () => {
     navigation.goBack();
@@ -37,24 +46,15 @@ export const VideosDetailScreen = ({ route, navigation }) => {
         accessoryLeft={BackAction}
       />
       <View style={{ flex: 1 }}>
-        {mounted ?
-
-          <Video
-            source={{ uri: videoDetail.address }}
-            shouldPlay
-            useNativeControls
-            resizeMode="contain"
-            ignoreSilentSwitch={'ignore'}
-            style={{ width: "100%", height: "100%", backgroundColor: "#000" }}
-          /> :
-          <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-          >
-            <Text category="h3" appearance="hint">
-              Loading...
-            </Text>
-          </View>
-        }
+        <Video
+          source={{ uri: videoDetail.address }}
+          shouldPlay
+          useNativeControls
+          resizeMode="contain"
+          style={{ width: "100%", height: "100%" }}
+          ref={ref}
+          onPlaybackStatusUpdate={(status) => setStatus(status)}
+        />
       </View>
     </View>
   );
